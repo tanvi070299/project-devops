887ef88dcb92ff8cc8cbf522405670c2f38945b0
Tanvi Thakur <39776349+tanvi070299@users.noreply.github.com>
2020-11-28 01:49:04 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'bolt';
+$db_username = 'id15525796_localhost';
+$db_password = 'Mridul@9560966699';
+$db_name = 'id15525796_root';
 $db_host = 'localhost';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
