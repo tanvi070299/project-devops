83aa42260d2b0a448c9a90710b654288c05028ce
Mridul Goyal <46794261+geekgod9@users.noreply.github.com>
2020-11-29 19:59:14 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'id15525796_localhost';
-$db_password = 'Mridul@9560966699';
-$db_name = 'id15525796_root';
-$db_host = 'localhost';
+$db_username = 'sql12379107';
+$db_password = 'tZQj1yFmff';
+$db_name = 'sql12379107';
+$db_host = 'sql12.freemysqlhosting.net';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
